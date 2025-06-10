f1eca6962cf10ac2ac000945531f023e7b23412e
Sidharth Tadeparti <139247354+sidharthtadeparti@users.noreply.github.com>
2025-06-10 16:11:18 -0700

    fix

--- run_annotation_script.sh
+++ run_annotation_script.sh
@@ -1,4 +1,4 @@
-python image_annotator.py \
+python image_annotater.py \
   --lora_path /app/CLIP-LoRA-Species-Mapping/models/bio_clip/google_cc/1000000shots/seed1/lora_weights.pt \
   --image_dir /app/DATASET/Rajasthan_Zones_v4_resampled/images \
   --output_file instances_default_new.json \
