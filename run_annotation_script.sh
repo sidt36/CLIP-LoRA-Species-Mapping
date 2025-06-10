af093fe47741dd8567498f9c23431487ea89e49d
Sidharth Tadeparti <139247354+sidharthtadeparti@users.noreply.github.com>
2025-06-10 16:10:27 -0700

    feat: run file

---
+++ run_annotation_script.sh
@@ -0,0 +1,6 @@
+python image_annotator.py \
+  --lora_path /app/CLIP-LoRA-Species-Mapping/models/bio_clip/google_cc/1000000shots/seed1/lora_weights.pt \
+  --image_dir /app/DATASET/Rajasthan_Zones_v4_resampled/images \
+  --output_file instances_default_new.json \
+  --encoder both \
+  --backbone bioclip
